e2a26879bdf582dd636eefca381cee06b0f39282
Anatolii Kamyshev <akamyshev93@gmail.com>
2023-05-22 18:25:53 -0500

    Project01 is added

---
+++ src/test/java/projects/Project_1.java
@@ -0,0 +1,20 @@
+package projects;
+
+public class Project_1 {
+    /* Test Case 01 - Validate the Contact Us information
+    Navigate to https://techglobal-training.com/frontend/project-1
+    Validate the heading is “Contact Us”
+    Validate the address is “2860 S River Rd Suite 350, Des Plaines IL 60018”
+    Validate the email is “info@techglobalschool.com"
+    Validate the phone number is “(773) 257-3010”
+     */
+
+    /* Test Case 02 - Validate the Full name input box
+
+     */
+
+
+
+
+
+}
